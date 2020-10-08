0a1a731572f3704616b55b8d6e192d22b615abaa
EtienneDuv <etienne.duverney@viacesi.fr>
2020-10-08 11:31:57 +0200

    front + post route

--- api/routes.js
+++ api/routes.js
@@ -1,7 +1,14 @@
-module.exports = function (app) {
+module.exports = async function (app) {
   app.get("/", (req, res) => {
     res.status(200);
     res.setHeader("Content-type", "text/html");
     res.render("index");
   });
+
+  app.post("/upload", (req, res) => {
+    console.log(req.body)
+    res.status(200);
+    res.setHeader("Content-type", "text/html");
+    res.render("index");
+  })
 }
