e0371c877233c302732571eddc096a7dd4dbfb69
EtienneDuv <etienne.duverney@viacesi.fr>
2020-10-11 11:43:10 +0200

    SHA1 functional

--- services/fileHandler.js
+++ services/fileHandler.js
@@ -1,23 +1,25 @@
 const Busboy = require('busboy');
-const sha1 = require('SHA1')
+const crypto = require('crypto');
 
 exports.handleFileUpload = (req) => {
   return new Promise((resolve, reject) => {
     try {
       let totalSize = 0;
-      let sha1Concat = ''
       const busboy = new Busboy({ headers: req.headers });
       busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
         console.log(`Filename:${filename}, encoding:${encoding}, mimetype:${mimetype}`);
+        const sha1 = crypto.createHash('sha1').setEncoding('hex');
         file.on('data', (data) => {
-          sha1Concat += sha1(data)
-          totalSize += data.length
+          sha1.update(data);
+          totalSize += data.length;
         });
         file.on('end', () => {
+          const finalSha = sha1.end().read()
+          console.log(finalSha)
           resolve(req.session.data.push({
             name: filename,
             size: totalSize,
-            sha1: sha1(sha1Concat)
+            sha1: finalSha
           }));
         });
       });
